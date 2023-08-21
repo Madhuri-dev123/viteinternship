bf0e7def78fd37be1ed41687a5be4c8fb78cf09b
Madhuri-div123 <gunnapananimadhuri@gmail.com>
2023-08-21 12:33:12 +0530

    madjhj

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,57 @@
+import './App.css'
 
+function App(){
+    const student =        [
+   {'firstname':'madhuri','lastname':'gunnapaneni',gender:'female'},
+   {'firstname':'manasa','lastname':'bathina',gender:'female'},
+   {'firstname':'vandana','lastname':'gunnapaneni',gender:'female'},
+   {'firstname':'gowri','lastname':'undavalli',gender:'female'},
+   {'firstname':'devagya','lastname':'undavalli',gender:'male'},
+   {'firstname':'nevas','lastname':'undavalli',gender:'male'},
+   
+   
+   
+   
 
-function App() {
+]
+  return(
+  <>
+  <button onClick={()=>{alert("hello button")}}>my btn</button>
+
+  <table border={2}>
+    <thead>
+      <tr>
+      <th>firstname</th>
+      <th>lastname</th>
+      <th>Gender</th>
+        </tr>
+    </thead>
+    <tbody>
+      {
+        student.map((student)=>{
+          return(<tr className={student.gender==='male'?"male":'female'}> 
+            <td>{student.firstname}</td>
+            <td>{student.lastname}</td>
+            <td>{student.gender}</td>
+          </tr>)
+
+        })
+        
+      }
+    </tbody>
+
+  </table>
+
   
+ 
 
-  return (
-    <>
-      <h1>welcome edupoly</h1>
-    </>
+
+
+
+     
+
+      </>
   )
+  
 }
-
 export default App
